506489b9d03ee8e49dfe595ba75c569a5b9292d2
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-06-05 20:51:48 -0500

    mejoras en la pagina de la carta de los productos de savalo

--- src/app/pages/carta/carta.component.ts
+++ src/app/pages/carta/carta.component.ts
@@ -6,10 +6,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carta.component.scss']
 })
 export class CartaComponent implements OnInit {
-
+  public carta: any[] = [];
   constructor() { }
 
   ngOnInit(): void {
+
+    this.getCarta(8);
+  }
+
+  public getCarta(numero) {
+
+      this.carta = [
+        {
+          id: 1,
+          img: `assets/carta/Carta Savalo-01.png`
+        },
+        {
+          id: 2,
+          img: `assets/carta/Carta Savalo-02.png`
+        },
+        {
+          id: 3,
+          img: `assets/carta/Carta Savalo-03.png`
+        },
+        {
+          id: 4,
+          img: `assets/carta/Carta Savalo-04.png`
+        },
+        {
+          id: 5,
+          img: `assets/carta/Carta Savalo-05.png`
+        },
+        {
+          id: 6,
+          img: `assets/carta/Carta Savalo-06.png`
+        },
+        {
+          id: 7,
+          img: `assets/carta/Carta Savalo-07.png`
+        },
+        {
+          id: 8,
+          img: `assets/carta/Carta Savalo-08.png`
+        },
+      ]
+    
+    
   }
 
 }

13d27012ef830c59bef4d5a0cddc6bb49618891a
Oliver Charry <oliver.charry.216@unisabaneta.edu.co>
2022-07-27 18:28:16 -0500

    ddddddd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_PRODUCTION: 'https://savalo-server-app.herokuapp.com/api/v1/',
+  API_PRODUCTION: 'https://savalo-server-back.herokuapp.com',
   API_LOCAL: 'http://localhost:4000/api/v1/',
 };
